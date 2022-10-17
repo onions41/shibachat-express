870e48e8bc8fa39932ba726ebd3ff96daf19b573
onions41 <kblee89@outlook.com>
2022-10-17 16:23:50 -0700

    Added the 2 test queries

--- src/resolvers/examples.js
+++ src/resolvers/examples.js
@@ -13,6 +13,20 @@ export default {
         return result
       }
       return null
+    },
+
+    protected: (_parent, _args, { req, res }) => {
+      console.log('*Protected Query*: ', 'Hi there******************')
+      console.log('*access-token*: ', req.headers['access-token'])
+      console.log('*refresh-token*: ', req.cookies['refresh-token'])
+      return true
+    },
+
+    unprotected: (_parent, _args, { req, res }) => {
+      console.log('**Unprotected Query**: ', 'Hi there**************************')
+      console.log('**access-token**: ', req.headers['access-token'])
+      console.log('**req.cookies.refresh-token**: ', req.cookies['refresh-token'])
+      return true
     }
   }
 }
