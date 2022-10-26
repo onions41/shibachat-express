566d4b511e62c31c3ad99d0d94723fa570451a99
onions41 <kblee89@outlook.com>
2022-10-25 21:29:04 -0700

    Tweeked authInput validation

--- src/inputValidation/authInput.js
+++ src/inputValidation/authInput.js
@@ -1,6 +1,6 @@
 import { object, string } from 'yup'
 
 export default object({
-  nickname: string().required().trim().min(2),
-  password: string().required().min(5)
+  nickname: string().required().trim().min(2).max(50),
+  password: string().required().min(5).max(100)
 })
