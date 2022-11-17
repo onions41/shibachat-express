58f8ad2cb5d1e5155c0696423e82b877c34975b9
onions41 <kblee89@outlook.com>
2022-11-16 21:35:18 -0800

    Added ReceivedFReqFromMe and status field to User and FRequest types

--- src/resolvers/user.js
+++ src/resolvers/user.js
@@ -26,36 +26,37 @@ export default {
   },
 
   User: {
-    sentFRequests: async ({ id }, _args, { prisma }) => {
-      // This SQL would work just as well.
-      // SELECT users.id, users.nickname
-      // FROM friend_requests JOIN users
-      // ON friend_requests.requestee_id = users.id
-      // WHERE friend_requests.requester_id = %i
-      // %i = id
+    // sentFRequests: async ({ id }, _args, { prisma }) => {
+    //   // This SQL would work just as well.
+    //   // SELECT users.id, users.nickname
+    //   // FROM friend_requests JOIN users
+    //   // ON friend_requests.requestee_id = users.id
+    //   // WHERE friend_requests.requester_id = %i
+    //   // %i = id
 
-      // Does not throw errors if it doesn't find anything. Just returns [].
-      const results = await prisma.friendRequest.findMany({
-        where: { requesterId: id },
-        select: {
-          requestee: {
-            select: {
-              id: true,
-              nickname: true
-            }
-          }
-        }
-      })
+    //   // Does not throw errors if it doesn't find anything. Just returns [].
+    //   const results = await prisma.friendRequest.findMany({
+    //     where: { requesterId: id },
+    //     select: {
+    //       requestee: {
+    //         select: {
+    //           id: true,
+    //           nickname: true
+    //         }
+    //       }
+    //     }
+    //   })
 
-      // Mapping to get at the nested User objects
-      return results.map((e) => e.requestee)
-    },
+    //   // Mapping to get at the nested User objects
+    //   return results.map((e) => e.requestee)
+    // },
 
     receivedFRequests: async ({ id }, _args, { prisma }) => {
       // Does not throw errors if it doesn't find anything. Just returns [].
-      const results = await prisma.friendRequest.findMany({
+      return await prisma.friendRequest.findMany({
         where: { requesteeId: id },
         select: {
+          status: true,
           requester: {
             select: {
               id: true,
@@ -64,9 +65,6 @@ export default {
           }
         }
       })
-
-      // Mapping to get at the nested User objects
-      return results.map((e) => e.requester)
     },
 
     receivedFReqFromMe: async ({ id }, _args, { prisma, meId }) => {
