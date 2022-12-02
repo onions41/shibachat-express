15684e1a248a07bb3100c443a7f75a856149c436
onions41 <kblee89@outlook.com>
2022-12-02 13:02:38 -0800

    Added id arg to user query

--- src/resolvers/queries.js
+++ src/resolvers/queries.js
@@ -1,14 +1,14 @@
 export default {
   Query: {
     // Fetches a single user
-    user: async (_parent, _args, { prisma, meId }) => {
-      const user = await prisma.user.findUnique({
-        where: { id: meId },
+    user: async (_parent, args, { prisma, meId }) => {
+      // If id was provided in the args, search for that user.
+      // Otherwise search for the current user (me).
+      const id = args.id ? args.id : meId
+      const user = await prisma.user.findUniqueOrThrow({
+        where: { id },
         select: { id: true, nickname: true }
       })
-      if (!user) {
-        throw new Error("***user Query could not find a user with that id")
-      }
       return user
     },
 
