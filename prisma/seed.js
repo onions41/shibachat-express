38c676b2b8f741942c372a8e84ccdeeffec9c062
onions41 <kblee89@outlook.com>
2023-01-28 20:44:16 -0800

    Added friends to db seed

--- prisma/seed.js
+++ prisma/seed.js
@@ -28,53 +28,79 @@ const userData = [
 
 const friendRequestData = [
   { senderId: 1, receiverId: 2, status: "ACCEPTED" },
-  { senderId: 2, receiverId: 1, status: "ACCEPTED" },
   { senderId: 1, receiverId: 3, status: "ACCEPTED" },
-  { senderId: 3, receiverId: 1, status: "ACCEPTED" },
   { senderId: 1, receiverId: 4, status: "ACCEPTED" },
-  { senderId: 4, receiverId: 1, status: "ACCEPTED" },
+  { senderId: 1, receiverId: 5, status: "ACCEPTED" },
+
+  { senderId: 2, receiverId: 1, status: "ACCEPTED" },
+  { senderId: 2, receiverId: 3, status: "ACCEPTED" },
+  { senderId: 2, receiverId: 4, status: "ACCEPTED" },
   { senderId: 2, receiverId: 5, status: "ACCEPTED" },
-  { senderId: 5, receiverId: 2, status: "ACCEPTED" },
+
+  { senderId: 3, receiverId: 1, status: "ACCEPTED" },
+  { senderId: 3, receiverId: 2, status: "ACCEPTED" },
   { senderId: 3, receiverId: 4, status: "ACCEPTED" },
-  { senderId: 4, receiverId: 3, status: "ACCEPTED" }
+  { senderId: 3, receiverId: 5, status: "ACCEPTED" },
+
+  { senderId: 4, receiverId: 1, status: "ACCEPTED" },
+  { senderId: 4, receiverId: 2, status: "ACCEPTED" },
+  { senderId: 4, receiverId: 3, status: "ACCEPTED" },
+  { senderId: 4, receiverId: 5, status: "ACCEPTED" },
+
+  { senderId: 5, receiverId: 1, status: "ACCEPTED" },
+  { senderId: 5, receiverId: 2, status: "ACCEPTED" },
+  { senderId: 5, receiverId: 3, status: "ACCEPTED" },
+  { senderId: 5, receiverId: 4, status: "ACCEPTED" }
 ]
 
 const friendData = [
   { userId: 1, friendId: 2 },
-  { userId: 2, friendId: 1 },
   { userId: 1, friendId: 3 },
-  { userId: 3, friendId: 1 },
   { userId: 1, friendId: 4 },
-  { userId: 4, friendId: 1 },
+  { userId: 1, friendId: 5 },
+
+  { userId: 2, friendId: 1 },
+  { userId: 2, friendId: 3 },
+  { userId: 2, friendId: 4 },
   { userId: 2, friendId: 5 },
-  { userId: 5, friendId: 2 },
+
+  { userId: 3, friendId: 1 },
+  { userId: 3, friendId: 2 },
   { userId: 3, friendId: 4 },
-  { userId: 4, friendId: 3 }
+  { userId: 3, friendId: 5 },
+
+  { userId: 4, friendId: 1 },
+  { userId: 4, friendId: 2 },
+  { userId: 4, friendId: 3 },
+  { userId: 4, friendId: 5 },
+
+  { userId: 5, friendId: 1 },
+  { userId: 5, friendId: 2 },
+  { userId: 5, friendId: 3 },
+  { userId: 5, friendId: 4 }
 ]
 
 async function main() {
   console.log("Start seeding ...")
 
   // User
-  for (const u of userData) {
-    await prisma.user.create({
-      data: u
-    })
-  }
+  await prisma.user.createMany({
+    data: userData,
+    skipDuplicates: true
+  })
 
   // FriendRequest
-  for (const fr of friendRequestData) {
-    await prisma.friendRequest.create({
-      data: fr
-    })
-  }
+
+  await prisma.friendRequest.createMany({
+    data: friendRequestData,
+    skipDuplicates: true
+  })
 
   // Friend
-  for (const f of friendData) {
-    await prisma.friend.create({
-      data: f
-    })
-  }
+  await prisma.friend.createMany({
+    data: friendData,
+    skipDuplicates: true
+  })
 
   console.log("Seeding finished.")
 }
